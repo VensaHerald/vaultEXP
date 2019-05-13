d147ea391e57dd3be0deecfb4e7b1cbb0c68daa0
Sam <19407947+VensaHerald@users.noreply.github.com>
2019-05-13 21:29:35 +0100

    set up class inheritance

--- vault_servs.py
+++ vault_servs.py
@@ -1,6 +1,8 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-
+import csv
+import time
+import sys
 
 
 class Google_Service:
@@ -12,19 +14,22 @@ class Google_Service:
     def create_service(self):
         creds = service_account.Credentials.from_service_account_file(self.file,scopes=self.scopes)
         delegated_creds = creds.with_subject(self.user)
-        service = build('vault', 'v1', credentials=delegated_creds)
+        self.service = build('vault', 'v1', credentials=delegated_creds)
         print("Service created")
-        return service
+        
     
     
 
-class Matter_Exports(Google_Service):
-    def __init__(self,service,matter_id):
+class Vault_Service(Google_Service):
+    def __init__(self, file, scopes, user, matter_id):
+        super().__init__(file, scopes, user)
         self.matter_id = matter_id
-        self.service = service
-        self.exports = service.matters().exports().list(matterId=matter_id).execute().get('exports',[])
+        
+    
+    def get_exports(self):
+        self.exports = self.service.matters().exports().list(matterId=self.matter_id).execute().get('exports',[])
         if self.exports:
-            print("Matter created")
+            print("Got Exports")
     
     def list_exports_id(self):
         export_list = []
@@ -43,7 +48,7 @@ class Matter_Exports(Google_Service):
         for export in self.list_exports_id():
             print("Export {}: Status is {}\n".format(export,self.status_export_by_id(export)))
     
-    def create_export(self,eml):
+    def create_1_export(self,eml):
         email_account = [eml]
         #query_options = {}
         query_terms = ''
@@ -58,6 +63,20 @@ class Matter_Exports(Google_Service):
             'exportOptions': {
                 'mailOptions':export_options
             }
-        }
+        } 
         # print(export_create)
         return self.service.matters().exports().create(matterId=self.matter_id,body =export_create).execute()
+    
+    def create_exports(self,path):
+        # Get file, exclude headers
+        email_list= [a for a in csv.reader(open(path,"r"))][1:]
+        #loop through email list and create an export per email.  Catch any failed exports in try/except
+        for email in email_list:
+            try:
+                created = va_matter.create_1_export(email[0]+"@remploy.co.uk")
+                print("export created for {}".format(email[0]))
+            except:
+                print("export failed for {}".format(email[0]))
+            time.sleep(120) # sleep to avoid goign over quota
+        return 0
+    
